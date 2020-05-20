89c42ff11a9dfc998d199bbb823d7c5e31c0663d
Ayush Munot <amunot6@gatech.edu>
2020-05-20 18:12:34 +0530

    firestore authentication added

---
+++ functions/APIs/todos.js
@@ -0,0 +1,23 @@
+const {db} = require('../utils/admin');
+exports.getAllTodos = (request, response) => {
+    db
+        .collection('todos')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let todos = [];
+            data.forEach((doc) => {
+                todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                });
+            });
+            return response.json(todos);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({error : err.code});
+        });
+    };
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const app = require('express')();
+const {
+    getAllTodos
+} = require('./APIs/todos')
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
