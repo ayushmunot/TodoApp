7d4e523277404785afce22c2e16657b0d4ae91fc
Ayush Munot <amunot6@gatech.edu>
2020-05-21 17:35:26 +0530

    fixed post method

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo
 } = require('./APIs/todos')
+app.post('/todo', postOneTodo);
 app.get('/todos', getAllTodos);
 exports.api = functions.https.onRequest(app);
